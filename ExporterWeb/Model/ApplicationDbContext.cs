431a31f786c8ae1c3dd8661abfc7a2ebf0728649
Sagid M <kofon95@mail.ru>
2020-06-17 15:53:07 +0300

    Add libraries, simple model and enable the C# 8 Nullable feature

---
+++ ExporterWeb/Model/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ExporterWeb.Model
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Dummy>? Dummies { get; set; }
+    }
+}

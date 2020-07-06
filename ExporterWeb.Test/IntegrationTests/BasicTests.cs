d524acf5b1bf41b71df5c488d64180b22632bc99
Magomed Mirzaev <magomed260298@gmail.com>
2020-07-06 15:53:27 +0000

    Merged in EX-19-добавить-интеграционные-тесты (pull request #19)
    EX-19 добавить интеграционные тесты
    
    * Add basic tests
    
    * Add tests in git hooks

---
+++ ExporterWeb.Test/IntegrationTests/BasicTests.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace ExporterWeb.Test.IntegrationTests
+{
+    public class BasicTests
+        : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly WebApplicationFactory<Startup> _factory;
+
+        public BasicTests(WebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Theory]
+        [InlineData("/")]
+        [InlineData("/Index")]
+        [InlineData("/Exporters")]
+        [InlineData("/Identity/Account/Register")]
+        public async Task Get_EndpointsReturnSuccessAndCorrectContentType(string url)
+        {
+            // Arrange
+            var client = _factory.CreateClient(
+                new WebApplicationFactoryClientOptions
+                {
+                    AllowAutoRedirect = false
+                });
+
+            // Act
+            var response = await client.GetAsync(url);
+
+            // Assert
+            response.EnsureSuccessStatusCode(); // Status Code 200-299
+            Assert.Equal("text/html; charset=utf-8",
+                response.Content.Headers.ContentType.ToString());
+        }
+
+        [Fact]
+        public async Task Get_SecurePageRedirectsAnUnauthenticatedUser()
+        {
+            // Arrange
+            var client = _factory.CreateClient(
+                new WebApplicationFactoryClientOptions
+                {
+                    AllowAutoRedirect = false
+                });
+
+            // Act
+            var response = await client.GetAsync("/Admin/FieldsOfActivity/Index");
+
+            // Assert
+            Assert.Equal(HttpStatusCode.Redirect, response.StatusCode);
+            Assert.Equal("/Identity/Account/Login",
+                response.Headers.Location.LocalPath);
+        }
+    }
+}

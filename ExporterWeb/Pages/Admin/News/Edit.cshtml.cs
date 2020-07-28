13f7a7b6da07c265c8c1b2a961e1aa9f557de325
Murad <1.murad.11@mail.ru>
2020-07-28 09:30:26 +0000

    Merged in feature/EX-39-добавить-обновление-файла-к-новост (pull request #37)
    Feature/EX-39 добавить обновление файла к новост
    
    * Update logo for news
    
    * Remove unnecessary usings
    
    * Replace removed image with default icon in edit news

--- ExporterWeb/Pages/Admin/News/Edit.cshtml.cs
+++ ExporterWeb/Pages/Admin/News/Edit.cshtml.cs
@@ -1,6 +1,9 @@
 ﻿using ExporterWeb.Areas.Identity.Authorization;
+using ExporterWeb.Helpers;
+using ExporterWeb.Helpers.Services;
 using ExporterWeb.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System.Threading.Tasks;
@@ -11,10 +14,12 @@ namespace ExporterWeb.Pages.Admin.News
     public class EditModel : BasePageModel
     {
         private readonly ApplicationDbContext _context;
+        private readonly ImageService _imageService;
 
-        public EditModel(ApplicationDbContext context)
+        public EditModel(ApplicationDbContext context, ImageService imageService)
         {
             _context = context;
+            _imageService = imageService;
         }
 
         public async Task<IActionResult> OnGetAsync(int? id)
@@ -30,7 +35,7 @@ namespace ExporterWeb.Pages.Admin.News
             return Page();
         }
 
-        public async Task<IActionResult> OnPostAsync(int id)
+        public async Task<IActionResult> OnPostAsync(int id, IFormFile? logo)
         {
             if (!ModelState.IsValid)
                 return Page();
@@ -39,15 +44,39 @@ namespace ExporterWeb.Pages.Admin.News
             if (newsItemToUpdate is null)
                 return NotFound();
 
+            var oldLogo = newsItemToUpdate.Logo;
+            if (logo is { })
+                newsItemToUpdate.Logo = _imageService.Save(ImageType.NewsLogo, logo);
+
             if (await TryUpdateModelAsync(
                     newsItemToUpdate,
                     "NewsItem",
                     n => n.Name, n => n.Description, n => n.Language))
             {
-                await _context.SaveChangesAsync();
+                try
+                {
+                    await _context.SaveChangesAsync();
+                    if (oldLogo is { })
+                        _imageService.Delete(ImageType.NewsLogo, oldLogo);
+                }
+                catch
+                {
+                    if (logo is { })
+                        _imageService.Delete(ImageType.NewsLogo, newsItemToUpdate.Logo!);
+                    throw;
+                }
             }
 
             return RedirectToPage("./Index");
+        }
+
+        public async Task<IActionResult> OnPostDeleteImage(int id)
+        {
+            NewsModel newsItem = await _context.News!.FindAsync(id);
+            _imageService.Delete(ImageType.NewsLogo, newsItem.Logo!);
+            newsItem.Logo = null;
+            await _context.SaveChangesAsync();
+            return StatusCode(StatusCodes.Status204NoContent);
         }
 
 #nullable disable

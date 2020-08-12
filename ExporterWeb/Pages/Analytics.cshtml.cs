0c7c11463912fe49b561145eec01a73bf5e2ec1f
Sagid Magomedov <kofon95@gmail.com>
2020-08-12 11:30:10 +0000

    Merged in feature/EX-56-добавить-шаблон-фронтенда-для-стат (pull request #52)
    Feature/EX-56-добавить-шаблон-фронтенда-для-стат
    
    * Add template for analytics page

---
+++ ExporterWeb/Pages/Analytics.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ExporterWeb.Pages
+{
+    public class AnalyticsModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ ExporterWeb/wwwroot/analytics-app/index.js
@@ -0,0 +1,19 @@
+﻿async function getData() {
+	return new Promise(res => setTimeout(res, 1000, {data: [1, 2, 3]}));
+}
+
+const Counter = {
+	data() {
+		return {
+			data: [],
+			loaded: false,
+		}
+	},
+	async mounted() {
+		const { data } = await getData();
+		this.data = data;
+		this.loaded = true;
+	}
+}
+
+Vue.createApp(Counter).mount('#statistics-app');

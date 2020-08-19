76978a78222fb43c5a7456ac5986a90742e08cc7
Sagid Magomedov <kofon95@gmail.com>
2020-08-19 11:58:57 +0000

    Merged in feature/EX-60-интегрировать-аналитику-с-сервером (pull request #54)
    Feature/EX-60 интегрировать аналитику с сервером
    
    * Add filtering by all the fields except countries
    
    * Move comment
    
    * Add countries.json and work with it
    
    * Add loading icon
    
    * Abort the last fetch for customs records before doing a new one

--- ExporterWeb/wwwroot/analytics-app/index.js
+++ ExporterWeb/wwwroot/analytics-app/index.js
@@ -1,19 +1,80 @@
-﻿async function getData() {
-	return new Promise(res => setTimeout(res, 1000, {data: [1, 2, 3]}));
+﻿const origin = window.parent.document.getElementById('origin-api').value;
+
+async function fetchCustomsRecords(signal, params) {
+	const url = `${origin}/?${new URLSearchParams(params)}`;
+	const resp = await fetch(url, { signal, cache: 'force-cache' });
+	return resp.json();
 }
 
-const Counter = {
+async function fetchRegions() {
+	const resp = await fetch('regions.json');
+	return resp.json();
+}
+
+async function fetchCountries() {
+	const resp = await fetch('countries.json');
+	return resp.json();
+}
+
+const main = {
 	data() {
 		return {
+			customsRecordsAbortController: new AbortController(),
 			data: [],
 			loaded: false,
+			maxRecordsToShow: 10,
+			allRegions: {},
+			allCountries: {},
+
+			dateFrom: '2017-01-01',
+			dateTo: null,
+			direction: '',
+			tnved: '',
+			regions: [],
+			// Not implemented yet
+			countries: [],
+		}
+	},
+	methods: {
+		async updateCustomsRecords() {
+			this.loaded = false;
+			const params = {};
+			if (this.dateFrom)
+				params.dateFrom = this.toDate(this.dateFrom);
+			if (this.dateTo)
+				params.dateTo = this.toDate(this.dateTo);
+			if (this.direction)
+				params.direction = this.direction;
+			if (this.tnved)
+				params.tnved = this.tnved;
+			if (this.regions.length !== 0)
+				params.regions = this.regions.join(',');
+			if (this.countries.length !== 0)
+				params.countries = this.countries.join(',');
+
+			this.customsRecordsAbortController.abort();
+			this.customsRecordsAbortController = new AbortController();
+			this.data = await fetchCustomsRecords(this.customsRecordsAbortController.signal, params);
+			this.loaded = true;
+		},
+		toDate(dateAsString) {
+			const date = new Date(dateAsString);
+			const year = date.getFullYear();
+			const month = (date.getMonth() + 1).toString().padStart(2, '0');
+			return `${year}${month}`;
+		},
+		numberOfRecordsToShow(records) {
+			const nums = Number(this.maxRecordsToShow);
+			if (nums >= records.length)
+				return records.length.toString();
+			return `${nums} / ${records.length}`;
 		}
 	},
 	async mounted() {
-		const { data } = await getData();
-		this.data = data;
-		this.loaded = true;
+		this.updateCustomsRecords();
+		this.allRegions = await fetchRegions();
+		this.allCountries = await fetchCountries();
 	}
 }
 
-Vue.createApp(Counter).mount('#statistics-app');
+const app = Vue.createApp(main).mount('#statistics-app');

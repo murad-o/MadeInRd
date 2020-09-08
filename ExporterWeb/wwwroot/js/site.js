6cf627631dd3bda6f96d8cac60bd0e17dc217fb0
Murad <1.murad.11@mail.ru>
2020-09-08 17:09:12 +0300

    Common styles for header

--- ExporterWeb/wwwroot/js/site.js
+++ ExporterWeb/wwwroot/js/site.js
@@ -1,4 +1,19 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿let burger = document.querySelector('.header__burger');
+burger.addEventListener('click', () => {
+    document.querySelector('.header__body').classList.toggle('active');
+});
 
-// Write your Javascript code.
+
+let searchButton = document.querySelector('.header__show-search-body');
+searchButton.addEventListener('click', () => {
+    document.querySelector('.header__search-body').classList.toggle('active');
+})
+
+
+let links = document.querySelectorAll('header li a');
+let urls = Array.from(links).map(item => new URL(item));
+let currentUrl = window.location;
+for (let i = 0; i < urls.length; i++) {
+    if (currentUrl.pathname.includes(urls[i].pathname))
+        links[i].parentNode.classList.add('selected');
+}

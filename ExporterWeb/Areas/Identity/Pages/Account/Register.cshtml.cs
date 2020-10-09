92802880d2dd4e3f1444483742306379b87cc07c
Murad <1.murad.11@mail.ru>
2020-10-09 15:07:15 +0300

    Fix conflict with registration

--- ExporterWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ ExporterWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -170,16 +170,7 @@ namespace ExporterWeb.Areas.Identity.Pages.Account
                 await _context.LanguageExporters!.AddAsync(languageExporter);
                 await _context.SaveChangesAsync();
 
-                var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
-                code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
-                var callbackUrl = Url.Page(
-                    "/Account/ConfirmEmail",
-                    pageHandler: null,
-                    values: new { area = "Identity", userId = user.Id, code, returnUrl },
-                    protocol: Request.Scheme);
-
-                await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
-                    $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+                await SendConfirmationEmail(user, returnUrl);
                 _context.Database.CommitTransaction();
                 if (_userManager.Options.SignIn.RequireConfirmedAccount)
                 {

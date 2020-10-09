4c7d84c8dbd4743ab17b1c2e7cf469065a0fc52c
Murad Omargadzhiev <murad@cron.ooo>
2020-10-09 11:41:38 +0000

    Merged in feature/MADEINRD-43-исправить-регистрацию (pull request #3)
    Feature/MADEINRD-43 исправить регистрацию
    
    Fix bug with registration

--- ExporterWeb/wwwroot/js/site.js
+++ ExporterWeb/wwwroot/js/site.js
@@ -72,3 +72,101 @@ popupUrls.forEach((url, index) => {
         document.querySelector('.big-menu__popup-toggler').classList.add('selected');
     }
 })
+
+
+$('[data-fancybox=""]').fancybox({
+    touch: false
+});
+
+
+new SlimSelect({
+    select: '#custom-select',
+    showSearch: true,
+    // searchText: 'Поиск',
+    searchPlaceholder: 'Поиск',
+    allowDeselect: true,
+    placeholder: 'Выберите отрасль'
+})
+
+
+// Показать/скрыть пароль
+function show_hide_password(event, id) {
+    let input = document.getElementById(id);
+
+    if (input.getAttribute('type') == 'password') {
+        event.currentTarget.classList.add('view');
+        input.setAttribute('type', 'text');
+    } else {
+        event.currentTarget.classList.remove('view');
+        input.setAttribute('type', 'password');
+    }
+}
+
+
+
+
+$(document).ready(function () {
+
+    $(".phone-mask").inputmask('+7 (999) 999-99-99');
+
+
+
+    /*
+    $('form.for-validate .required-input').on('change', function () {
+        let canSubmit = true;
+        $(this).attr('data-changed', true);
+        $(this).parents('form.for-validate').find('.required-input').each(function () {
+            if ($(this).hasClass('phone-mask') || $(this).hasClass('email-mask')) {
+                if (!$(this).inputmask("isComplete")) {
+                    canSubmit = false;
+                    if ($(this).attr('data-changed')) {
+                        $(this).parents('.form-group').find('.input-group.required').addClass('alert-required');
+                    }
+                } else {
+                    $(this).parents('.form-group').find('.input-group.required').removeClass('alert-required')
+                }
+            } else {
+                if ($(this).hasClass('custom-control-input') && $(this).prop('checked') !== true) {
+                    canSubmit = false;
+                } else {
+                    if (!$(this).hasClass('custom-control-input')) {
+                        if (!$(this).val()) {
+                            canSubmit = false;
+                            if ($(this).attr('data-changed')) {
+                                $(this).parents('.form-group').find('.input-group.required').addClass('alert-required');
+                                if ($(this).is('select') || $(this).hasClass('dp')) {
+                                    $(this).next().find('.select2-selection.select2-selection--single').addClass('alert-border');
+                                    $(this).parents('.form-group').css({
+                                        'z-index': 1,
+                                        'position': 'relative',
+                                    })
+                                }
+                                if ($(this).hasClass('dp')) {
+                                    $(this).addClass('alert-border');
+                                }
+                            }
+                        } else {
+                            $(this).parents('.form-group').find('.input-group.required').removeClass('alert-required')
+                        }
+                    }
+                }
+            }
+        });
+
+        if (canSubmit) {
+            $(this).parents('form.for-validate').find('a.form-submit').removeClass('disabled').parent()
+                .removeAttr('data-toggle')
+                .removeAttr('data-placement')
+                .removeAttr('data-title')
+                .tooltip('disable');
+        } else {
+            $(this).parents('form.for-validate').find('a.form-submit').addClass('disabled').parent()
+                .attr('data-toggle', 'tooltip')
+                .attr('data-placement', 'top')
+                .attr('data-title', 'Пожалуйста, заполните все обязательные поля')
+                .tooltip('enable');
+        }
+    });
+     */
+
+});

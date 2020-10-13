02c15ae3474c50aba401adb58d38ca1abd91ee39
Murad <1.murad.11@mail.ru>
2020-10-13 11:53:33 +0300

    Add TermsOfUse page

---
+++ ExporterWeb/Pages/TermsOfUse.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ExporterWeb.Pages
+{
+    public class TermsOfUse : PageModel
+    {
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+    }
+}

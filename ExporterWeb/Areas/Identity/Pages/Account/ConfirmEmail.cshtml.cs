9afb19cfcbc3ee8b0e270cf5a8e40c2aa6f7789a
Magomed Mirzaev <mmr@cron.ooo>
2020-10-14 07:49:06 +0000

    Merged in feature/MADEINRD-46-сверстать-страницу-подтвержд (pull request #9)
    Feature/MADEINRD-46 сверстать страницу подтвержд
    
    Add layout for EmailConfirmed page

--- ExporterWeb/Areas/Identity/Pages/Account/ConfirmEmail.cshtml.cs
+++ ExporterWeb/Areas/Identity/Pages/Account/ConfirmEmail.cshtml.cs
@@ -1,14 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ExporterWeb.Models;
 using Microsoft.AspNetCore.Authorization;
-using ExporterWeb.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace ExporterWeb.Areas.Identity.Pages.Account
 {

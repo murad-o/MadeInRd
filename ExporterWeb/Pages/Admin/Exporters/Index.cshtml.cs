597f518a5e86d2cb11d36cb8338a4e86e423d741
Murad Omargadzhiev <murad@cron.ooo>
2020-10-21 08:52:28 +0000

    Merged in feature/MADEINRD-129-добавить-страницу-в-админке (pull request #39)
    Feature/MADEINRD-129 добавить страницу в админке
    
    Add opportunity to approve exporters for admin

---
+++ ExporterWeb/Pages/Admin/Exporters/Index.cshtml.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+using ExporterWeb.Areas.Identity.Authorization;
+using ExporterWeb.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExporterWeb.Pages.Admin.Exporters
+{
+    [Authorize(Roles = Constants.AdministratorsRole)]
+    public class Index : PageModel
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IEmailSender _emailSender;
+
+        public Index(ApplicationDbContext context, IEmailSender emailSender)
+        {
+            _context = context;
+            _emailSender = emailSender;
+            Exporters = _context.LanguageExporters!
+                .Where(exporter => !exporter.Approved && exporter.Language == CultureInfo.CurrentCulture.TwoLetterISOLanguageName).ToList();
+        }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            var exporter = await _context.LanguageExporters
+                .Include(e => e.CommonExporter)
+                .Include(e => e.CommonExporter.User)
+                .FirstOrDefaultAsync(e => e.CommonExporterId == Id && e.Language == Language);
+
+            exporter.Approved = true;
+            await _context.SaveChangesAsync();
+            var email = exporter.CommonExporter!.User!.Email;
+            const string message =
+                "Администрация Made in RD проверила информация вашей компании и одобрила ее. Теперь вы можете полноценно пользоваться всеми возможностями сайта";
+            string subject = "Компания одобрена";
+
+            await _emailSender.SendEmailAsync(email, subject, message);
+            return Page();
+        }
+
+
+#nullable disable
+        public List<LanguageExporter> Exporters { get; set; }
+        [BindProperty]
+        public string Id { get; set; }
+        [BindProperty]
+        public string Language { get; set; }
+        [BindProperty]
+        public string RejectionReason { get; set; }
+    }
+}
---
+++ ExporterWeb/Pages/Admin/Exporters/Reject.cshtml.cs
@@ -0,0 +1,53 @@
+﻿using System.Globalization;
+using System.Threading.Tasks;
+using ExporterWeb.Areas.Identity.Authorization;
+using ExporterWeb.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace ExporterWeb.Pages.Admin.Exporters
+{
+    [Authorize(Roles = Constants.AdministratorsRole)]
+    public class Reject : PageModel
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly IEmailSender _emailSender;
+
+        public Reject(ApplicationDbContext context, IEmailSender emailSender)
+        {
+            _context = context;
+            _emailSender = emailSender;
+        }
+
+        public void OnGet(string id, string language)
+        {
+            Id = id;
+            Language = language;
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            var exporter = await _context.LanguageExporters
+                .Include(e => e.CommonExporter)
+                .Include(e => e.CommonExporter.User)
+                .FirstOrDefaultAsync(e => e.CommonExporterId == Id && e.Language == CultureInfo.CurrentCulture.TwoLetterISOLanguageName);
+
+            var email = exporter.CommonExporter!.User!.Email;
+            string message = $"Ваша компания не одобрена администрацией сайта. Причина отказа: {RejectionReason}";
+            string subject = "Компания отклонена";
+            await _emailSender.SendEmailAsync(email, subject, message);
+
+            return RedirectToPage("./Index");
+        } 
+        
+        [BindProperty]
+        public string Id { get; set; }
+        [BindProperty]
+        public string RejectionReason { get; set; }
+        [BindProperty]
+        public string Language { get; set; }
+    }
+}

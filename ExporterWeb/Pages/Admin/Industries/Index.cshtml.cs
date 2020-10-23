2df9455da427ae89e62161fb05fe7cda293dec46
Magomed Mirzaev <mmr@cron.ooo>
2020-10-23 14:51:47 +0000

    Merged in feature/MADEINRD-179-создать-страницу-списка-отр (pull request #56)
    feature/MADEINRD-179-создать-страницу-списка-отр
    
    Add industries list page for admin

---
+++ ExporterWeb/Pages/Admin/Industries/Index.cshtml.cs
@@ -0,0 +1,15 @@
+﻿using ExporterWeb.Areas.Identity.Authorization;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ExporterWeb.Pages.Admin.Industries
+{
+    [Authorize(Roles = Constants.AdministratorsRole)]
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}

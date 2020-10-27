deb1e202139a5b29ee13b58ed4c8c27029fa2878
Magomed Mirzaev <mmr@cron.ooo>
2020-10-27 07:51:15 +0000

    Merged in feature/MADEINRD-180-создать-страницу-отдельной- (pull request #61)
    Feature/MADEINRD-180 создать страницу отдельной
    
    * Add Industries index page
    
    * Add details page for industries

---
+++ ExporterWeb/Pages/Industries/Details.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ExporterWeb.Pages.Industries
+{
+    public class DetailsModel : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}
---
+++ ExporterWeb/Pages/Industries/Index.cshtml.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ExporterWeb.Pages.Industries
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+            
+        }
+    }
+}

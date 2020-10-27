d9342a93042ae775095632e7b4f19ee28ced65c2
Murad Omargadzhiev <murad@cron.ooo>
2020-10-27 16:39:12 +0000

    Merged in feature/MADEINRD-200-вынести-inputmodel-из-регис (pull request #64)
    Move register InputModel to ViewModel

--- ExporterWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ ExporterWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -8,12 +8,11 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
-using AccountResources = ExporterWeb.Resources.Account.Account;
+using ExporterWeb.Models.ViewModels;
+
 
 namespace ExporterWeb.Areas.Identity.Pages.Account
 {
@@ -42,75 +41,6 @@ namespace ExporterWeb.Areas.Identity.Pages.Account
 
         public IEnumerable<IndustryTranslation> Industries => _context.IndustryTranslations!
             .Where(i => i.Language == CultureInfo.CurrentCulture.TwoLetterISOLanguageName).ToList();
-
-        public class InputModel
-        {
-            // User form 
-            [Required(ErrorMessage = "This field is required")]
-            [EmailAddress(ErrorMessage = "Invalid email address")]
-            [Display(Name = "Email", ResourceType = typeof(AccountResources))]
-            public string Email { get; set; } = "";
-
-            [Required(ErrorMessage = "This field is required")]
-            [StringLength(100, ErrorMessage = "Passwords must consist at least {2} characters", MinimumLength = 6)]
-            [DataType(DataType.Password)]
-            [Display(Name = "Password", ResourceType = typeof(AccountResources))]
-            public string Password { get; set; } = "";
-
-            [DataType(DataType.Password)]
-            [Display(Name = "ConfirmPassword", ResourceType = typeof(AccountResources))]
-            [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
-            [Required(ErrorMessage = "This field is required")]
-            public string ConfirmPassword { get; set; } = "";
-
-            // CommonExporter form
-            [Required(ErrorMessage = "This field is required")]
-            [RegularExpression(@"^(\d{10}|\d{12})$", ErrorMessage = "This field must consist 10 or 12 digits")]
-            [StringLength(12, MinimumLength = 10)]
-            [Display(Name = "INN", ResourceType = typeof(AccountResources))]
-            public string INN { get; set; } = "";
-
-            [Required(ErrorMessage = "This field is required")]
-            [RegularExpression(@"^(\d{13}|\d{15})$", ErrorMessage = "This field must consist 13 or 15 digits")]
-            [StringLength(15, MinimumLength = 13)]
-            [Display(Name = "OGRN_IP", ResourceType = typeof(AccountResources))]
-            public string OGRN_IP { get; set; } = "";
-
-            [Required(ErrorMessage = "This field is required")]
-            [Display(Name = "Industry", ResourceType = typeof(AccountResources))]
-            public int IndustryId { get; set; }
-
-            // LanguageExporter form
-            [Required(ErrorMessage = "This field is required")]
-            [Display(Name = "Name", ResourceType = typeof(AccountResources))]
-            public string Name { get; set; } = "";
-
-            public string? Description { get; set; }
-
-            [Required(ErrorMessage = "This field is required")]
-            [Display(Name = "FirstName", ResourceType = typeof(AccountResources))]
-            
-            public string ContactPersonFirstName { get; set; } = "";
-            [Required(ErrorMessage = "This field is required")]
-            [Display(Name = "LastName", ResourceType = typeof(AccountResources))]
-            public string ContactPersonSecondName { get; set; } = "";
-            [Display(Name = "Patronymic", ResourceType = typeof(AccountResources))]
-            public string? ContactPersonPatronymic { get; set; }
-
-            [Required(ErrorMessage = "This field is required")]
-            [Display(Name = "Position", ResourceType = typeof(AccountResources))]
-            public string Position { get; set; } = "";
-
-            [Required(ErrorMessage = "This field is required")]
-            [Display(Name = "Phone", ResourceType = typeof(AccountResources))]
-            [DataType(DataType.PhoneNumber)]
-            [RegularExpression(@"^\+7\s\(\d{3}\)\s\d{3}-\d{2}-\d{2}$", ErrorMessage = "Enter a valid phone")]
-            public string Phone { get; set; } = "";
-
-            [Required]
-            [Range(typeof(bool), "true", "true", ErrorMessage = "Your consent with the user agreement is required")]
-            public bool IsTermsOfUseAgreed { get; set; }
-        }
 
         public async Task OnGetAsync(string? returnUrl)
         {
@@ -192,11 +122,9 @@ namespace ExporterWeb.Areas.Identity.Pages.Account
             return Page();
         }
 
-        
-
 
 #nullable disable
         [BindProperty]
-        public InputModel Input { get; set; }
+        public RegisterViewModel Input { get; set; }
     }
 }
---
+++ ExporterWeb/Models/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,74 @@
+﻿using System.ComponentModel.DataAnnotations;
+using AccountResources = ExporterWeb.Resources.Account.Account;
+
+namespace ExporterWeb.Models.ViewModels
+{
+    public class RegisterViewModel
+    {
+        // User form 
+            [Required(ErrorMessage = "This field is required")]
+            [EmailAddress(ErrorMessage = "Invalid email address")]
+            [Display(Name = "Email", ResourceType = typeof(AccountResources))]
+            public string Email { get; set; } = "";
+
+            [Required(ErrorMessage = "This field is required")]
+            [StringLength(100, ErrorMessage = "Passwords must consist at least {2} characters", MinimumLength = 6)]
+            [DataType(DataType.Password)]
+            [Display(Name = "Password", ResourceType = typeof(AccountResources))]
+            public string Password { get; set; } = "";
+
+            [DataType(DataType.Password)]
+            [Display(Name = "ConfirmPassword", ResourceType = typeof(AccountResources))]
+            [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+            [Required(ErrorMessage = "This field is required")]
+            public string ConfirmPassword { get; set; } = "";
+
+            // CommonExporter form
+            [Required(ErrorMessage = "This field is required")]
+            [RegularExpression(@"^(\d{10}|\d{12})$", ErrorMessage = "This field must consist 10 or 12 digits")]
+            [StringLength(12, MinimumLength = 10)]
+            [Display(Name = "INN", ResourceType = typeof(AccountResources))]
+            public string INN { get; set; } = "";
+
+            [Required(ErrorMessage = "This field is required")]
+            [RegularExpression(@"^(\d{13}|\d{15})$", ErrorMessage = "This field must consist 13 or 15 digits")]
+            [StringLength(15, MinimumLength = 13)]
+            [Display(Name = "OGRN_IP", ResourceType = typeof(AccountResources))]
+            public string OGRN_IP { get; set; } = "";
+
+            [Required(ErrorMessage = "This field is required")]
+            [Display(Name = "Industry", ResourceType = typeof(AccountResources))]
+            public int IndustryId { get; set; }
+
+            // LanguageExporter form
+            [Required(ErrorMessage = "This field is required")]
+            [Display(Name = "Name", ResourceType = typeof(AccountResources))]
+            public string Name { get; set; } = "";
+
+            public string? Description { get; set; }
+
+            [Required(ErrorMessage = "This field is required")]
+            [Display(Name = "FirstName", ResourceType = typeof(AccountResources))]
+            
+            public string ContactPersonFirstName { get; set; } = "";
+            [Required(ErrorMessage = "This field is required")]
+            [Display(Name = "LastName", ResourceType = typeof(AccountResources))]
+            public string ContactPersonSecondName { get; set; } = "";
+            [Display(Name = "Patronymic", ResourceType = typeof(AccountResources))]
+            public string? ContactPersonPatronymic { get; set; }
+
+            [Required(ErrorMessage = "This field is required")]
+            [Display(Name = "Position", ResourceType = typeof(AccountResources))]
+            public string Position { get; set; } = "";
+
+            [Required(ErrorMessage = "This field is required")]
+            [Display(Name = "Phone", ResourceType = typeof(AccountResources))]
+            [DataType(DataType.PhoneNumber)]
+            [RegularExpression(@"^\+7\s\(\d{3}\)\s\d{3}-\d{2}-\d{2}$", ErrorMessage = "Enter a valid phone")]
+            public string Phone { get; set; } = "";
+
+            [Required]
+            [Range(typeof(bool), "true", "true", ErrorMessage = "Your consent with the user agreement is required")]
+            public bool IsTermsOfUseAgreed { get; set; }
+    }
+}

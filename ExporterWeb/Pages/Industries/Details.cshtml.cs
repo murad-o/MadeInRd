c999e77b86d054e44c6f8090c7b3e4b14922de19
Худатбегов Магомед <hmm@cron.ooo>
2020-11-05 11:17:50 +0000

    Merged in MADEINRD-182-сверстать-страницу-отдельно (pull request #80)
    MADEINRD-182 сверстать страницу отдельно
    
    Add industry details page

--- ExporterWeb/Pages/Industries/Details.cshtml.cs
+++ ExporterWeb/Pages/Industries/Details.cshtml.cs
@@ -1,12 +1,25 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using System.Threading.Tasks;
+using ExporterWeb.Models;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace ExporterWeb.Pages.Industries
 {
     public class DetailsModel : PageModel
     {
-        public void OnGet()
+        private readonly ApplicationDbContext _context;
+
+        public DetailsModel(ApplicationDbContext context)
         {
-            
+            _context = context;
         }
+
+        public async Task OnGet(int id)
+        {
+            Industry = await _context.IndustryTranslations.FirstOrDefaultAsync(i => i.Id == id);
+        }
+        
+        #nullable disable
+        public IndustryTranslation Industry { get; set; }
     }
 }

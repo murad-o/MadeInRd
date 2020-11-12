d3f60f26dce627700c3de9a4560eb12fc0ae4128
Murad Omargadzhiev <murad@cron.ooo>
2020-11-12 10:41:06 +0000

    Merged in feature/MADEINRD-275-создать-страницу-карточки-э (pull request #112)
    Add empty exporter info page for admin

---
+++ ExporterWeb/Pages/Admin/Exporters/Details.cshtml.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ExporterWeb.Pages.Admin.Exporters
+{
+    public class Edit : PageModel
+    {
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+    }
+}

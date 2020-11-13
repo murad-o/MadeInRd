a2203ac14843922c7155d6ef37670c09f9352179
Murad Omargadzhiev <murad@cron.ooo>
2020-11-13 13:45:22 +0000

    Merged in feature/MADEINRD-276-реализовать-логику-изменени (pull request #116)
    Feature/MADEINRD-276 реализовать логику изменени
    
    * Add email notification when admin change status of an exporter

--- ExporterWeb/Pages/Admin/Exporters/Details.cshtml.cs
+++ ExporterWeb/Pages/Admin/Exporters/Details.cshtml.cs
@@ -1,14 +1,112 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
+using ExporterWeb.Helpers;
+using ExporterWeb.Helpers.Services;
+using ExporterWeb.Models;
+using ExporterWeb.Models.ViewModels;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
 namespace ExporterWeb.Pages.Admin.Exporters
 {
-    public class Edit : PageModel
+    public class Details : PageModel
     {
-        public IActionResult OnGet()
+        private readonly ApplicationDbContext _context;
+        private readonly IEmailSender _emailSender;
+        private readonly RazorPartialToStringRenderer _partialToStringRenderer;
+
+        public Details(ApplicationDbContext context, IEmailSender emailSender, RazorPartialToStringRenderer partialToStringRenderer)
         {
+            _context = context;
+            _emailSender = emailSender;
+            _partialToStringRenderer = partialToStringRenderer;
+        }
+
+        public async Task<IActionResult> OnGetAsync(string? id)
+        {
+            if (id is null)
+            {
+                return NotFound();
+            }
+
+            LanguageExporter = await _context.LanguageExporters!
+                .Include(e => e.CommonExporter)
+                .FirstOrDefaultAsync(e => e.CommonExporterId == id && e.Language == Languages.DefaultLanguage);
+
             return Page();
         }
+
+        public async Task<IActionResult> OnPostAsync(string id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            LanguageExporter = await _context.LanguageExporters!
+                .Include(e => e.CommonExporter)
+                .Include(e => e.CommonExporter!.User)
+                .FirstOrDefaultAsync(e => e.CommonExporterId == id && e.Language == Languages.DefaultLanguage);
+
+            if (LanguageExporter is null)
+            {
+                return NotFound();
+            }
+
+            LanguageExporter!.CommonExporter!.Status = Status.ToString();
+            await _context.SaveChangesAsync();
+            await SendStatusChangedMessage(LanguageExporter);   
+
+            return RedirectToPage("./Index");
+        }
+
+        private async Task SendStatusChangedMessage(LanguageExporter exporter)
+        {
+            if (Status == ExporterStatus.Approved)
+            {
+                await SendApprovedStatusMessage(LanguageExporter);
+            }
+            else if (Status == ExporterStatus.Refused) 
+            {
+                await SendRefusedStatusMessage(LanguageExporter);
+            }
+        }
+
+        private async Task SendApprovedStatusMessage(LanguageExporter exporter)
+        {
+            var email = exporter.CommonExporter!.User!.Email;
+            var body =  await _partialToStringRenderer.RenderPartialToStringAsync(
+                "Emails/AccountApprovedNotificationEmail",
+                new ContactInfoModel
+                    { FirstName = exporter.ContactPersonFirstName, LastName = exporter.ContactPersonSecondName });
+
+            await _emailSender.SendEmailAsync(email, "Аккаунт одобрен", body);
+        }
+
+        private async Task SendRefusedStatusMessage(LanguageExporter exporter)
+        {
+            var email = exporter.CommonExporter!.User!.Email;
+            var body = await _partialToStringRenderer.RenderPartialToStringAsync(
+                "Emails/AccountRefusedNotificationEmail",
+                new AccountRefusedViewModel
+                {
+                    FirstName = exporter.ContactPersonFirstName,
+                    LastName = exporter.ContactPersonSecondName,
+                    RejectionReason = RejectionReason
+                });
+            await _emailSender.SendEmailAsync(email, "Аккаунт отклонен", body);
+        }
+        
+
+#nullable disable
+        public LanguageExporter LanguageExporter { get; set; }
+        
+        [BindProperty]
+        public ExporterStatus Status { get; set; }
+
+        [BindProperty]
+        public string RejectionReason { get; set; }
     }
 }

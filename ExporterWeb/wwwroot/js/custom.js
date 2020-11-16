b9fe477f015e45e4eda97c2b9983a7c2d8096db4
Murad Omargadzhiev <murad@cron.ooo>
2020-11-16 15:04:06 +0000

    Merged in feature/MADEINRD-252-указать-формат-jpg-png-для- (pull request #111)
    validate file input

---
+++ ExporterWeb/wwwroot/js/custom.js
@@ -0,0 +1,11 @@
+﻿// Валидация на загрузку изображения
+let fileInput = document.getElementById('file-input');
+fileInput.addEventListener('change', () => {
+    const validExtensions = ['.jpg', '.png', '.jpeg'];
+    let fileName = fileInput.files[0].name;
+    let fileExtension = fileName.substr(fileName.lastIndexOf('.'));
+    if (!validExtensions.includes(fileExtension)) {
+        fileInput.value = "";
+        alert("Принимаются только следующие типы файлов: " + validExtensions.join(', '));
+    }
+});

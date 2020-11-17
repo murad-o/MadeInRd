e90ae42ee56caa42989e5242e8e95d1f47089328
Murad Omargadzhiev <murad@cron.ooo>
2020-11-17 07:38:36 +0000

    Merged in feature/MADEINRD-287-перенести-скрипты-с-изображ (pull request #123)
    Move 2 scripts with file loading into 1 file

--- ExporterWeb/wwwroot/js/custom.js
+++ ExporterWeb/wwwroot/js/custom.js
@@ -1,11 +1,35 @@
 ﻿// Валидация на загрузку изображения
 let fileInput = document.getElementById('file-input');
-fileInput.addEventListener('change', () => {
+fileInput?.addEventListener('change', () => {
     const validExtensions = ['.jpg', '.png', '.jpeg'];
     let fileName = fileInput.files[0].name;
     let fileExtension = fileName.substr(fileName.lastIndexOf('.'));
+
     if (!validExtensions.includes(fileExtension)) {
         fileInput.value = "";
         alert("Принимаются только следующие типы файлов: " + validExtensions.join(', '));
+    }
+});
+
+
+// Удаление изображения
+let deleteImageForm = document.getElementById('delete-image-form');
+deleteImageForm?.addEventListener('submit', async function(e) {
+    e.preventDefault();
+
+    if (!confirm('Вы действительно хотите удалить изображение?'))
+        return;
+    const handlerParam = 'handler=deleteimage';
+    const url = location.search ? `${location.search}&${handlerParam}` : `?${handlerParam}`;
+    const result = await fetch(url, {
+        method: 'post',
+        body: new FormData(this)
+    })
+
+    if (result.ok) {
+        document.getElementById('current-image-row').style.display = 'none';
+        document.querySelector('.upload-img-block').style.display = "block";
+    } else {
+        alert('Неизвестная ошибка: ' + result.statusText);
     }
 });

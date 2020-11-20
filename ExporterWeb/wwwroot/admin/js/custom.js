d2e9a63520927dcf8f119b59663c45e48f3ed3d7
Murad Omargadzhiev <murad@cron.ooo>
2020-11-20 09:21:20 +0000

    Merged in feature/MADEINRD-307-добавить-навигацию-в-выпада (pull request #136)
    Add navigation for exporters link for admin

---
+++ ExporterWeb/wwwroot/admin/js/custom.js
@@ -0,0 +1,6 @@
+﻿let exportersLink = document.querySelectorAll('.exporters-link');
+exportersLink.forEach(item => {
+    if (item.pathname === location.pathname) {
+       item.classList.add('active');
+    }
+});
